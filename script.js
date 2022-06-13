a76604d3c1b9a99418f6d859f72b3fb7fd27c58c
Shivateja Gondala <94845621+shivateja24@users.noreply.github.com>
2022-06-14 02:12:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,69 @@
-
+var gamelevel=2;
+var level=document.querySelector('.level');
+level.innerHTML= "LEVEL:"+gamelevel;
 var buttons = document.querySelectorAll("button")
 var clicked=[];
 var blinkedindex=[];
 var buttonsclassName=[];
+var blinked=[];
+click(buttons,clicked);
+
 console.log(buttons);
+
 buttons.forEach(function(button){buttonsclassName.push(button.className)})
-for(i=0;i<=16;i++)
+for(let i=0;i<16;i++)
 {var s=Math.floor(Math.random()*16);
-blinkedindex.push(s);}
-var level;
-for(level=0;level<20;level++)
-{for(let i=0;i<level;i++)
-{var k=blinkedindex[i];
-    blink(buttons[k]);
+blinkedindex.push(s);
+blinked.push(buttons[s].className)}
+var k=0;
+
+clear=setInterval(blinkoneafteranother,1000,buttons,gamelevel,blinkedindex,buttonsclassName);
+
+
+setTimeout(result,10000,blinked,clicked,gamelevel);
+
+
+
+
+
+
+function blinkoneafteranother(buttons,gamelevel,blinkedindex,buttonsclassName)
+{  console.log(k);     
+    var s=blinkedindex[k];
+    buttons[s].className="blink";
+    setTimeout(stopblink,900,buttons,buttonsclassName);
+   k++;
+   if(k>=gamelevel)
+   {clearInterval(clear);}
 }
-setTimeout(click,3000);
-setTimeout(stopblink,3000,buttons);
-setTimeout(result,33000);
-}
 
 
-function stopblink(arr,i)
-{ arr.forEach(function(item){item.className=blinkedindex[i];
- i++;})}
 
- function blink(e)
-{e.className="blink";}
-function click()
-{
-    buttons.forEach(function(button){button.addEventListener('click',function(){clicked.push(button.className);})})
-}
 
-function result(blinked,clicked)
 
-{if(JSON.stringify(blinked) == JSON.stringify(clicked))
-    {alert("You Won");
+
+function stopblink(arr,k)
+{ for(i=0;i<16;i++)
+{arr[i].className=k[i];
+}}
+
+ function blink(button)
+{button.className.add("blink");}
+
+
+
+function result(blinked,clicked,i)
+
+{console.log(blinked);
+ console.log(clicked);
+    for(var j=0;j<gamelevel;j++)
+    {if(blinked[j] != clicked[j])
+    {alert("You lost");
+    
+    location.reload();
+     }}
+     alert("you won");
      }
- }
-
+ 
+function click(buttons,clicked)
+{buttons.forEach(function(button){button.addEventListener('click',function(){clicked.push(button.className);console.log(button.className);})});}
